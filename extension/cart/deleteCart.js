0ee7a1396143e6f32769b0130d18f65653719889
rutkowski <waldemar.rutkowski@shopgate.com>
2018-04-26 13:39:07 +0200

    PWA-199: Removed redundant backup of the cart on deletion. Updated unit tests and split them into multiple ones. Added the storage del method to the context mock method.

--- extension/cart/deleteCart.js
+++ extension/cart/deleteCart.js
@@ -6,27 +6,9 @@ const InternalError = require('../common/Error/InternalError')
  * @param {SDKContext} context
  * @param {Object} input
  * @param {string} input.cartStorageName
+ * @returns {Promise<undefined>}
  */
 module.exports = async (context, input) => {
-  let cart
-
-  // 1. Get a cart
-  try {
-    cart = await context.storage[input.cartStorageName].get('cart')
-  } catch (err) {
-    context.log.error(err, `Failed to load a cart from ${input.cartStorageName} storage`)
-    throw new InternalError()
-  }
-
-  // 2. Backup a cart
-  try {
-    await context.storage[input.cartStorageName].set('cart_bak', cart)
-  } catch (err) {
-    context.log.error(err, `Failed to backup to ${input.cartStorageName} storage`)
-    throw new InternalError()
-  }
-
-  // 3. Delete a cart
   try {
     await context.storage[input.cartStorageName].del('cart')
   } catch (err) {
--- extension/test/unit/cart/deleteCart-test.js
+++ extension/test/unit/cart/deleteCart-test.js
@@ -1,50 +1,82 @@
 const assert = require('assert')
-const {PRODUCT, COUPON} = require('../../../common/consts')
+const { createInput, createContext } = require('../sdkHelper')
+const InternalError = require('../../../common/Error/InternalError')
 const executeStep = require('../../../cart/deleteCart')
 
-describe('deleteCart after checkout', () => {
+describe('deleteCart', () => {
   const cartStorageName = 'user'
 
-  const cart = [
-    {
-      id: 'qwerty123',
-      productId: 'QWERTY123',
-      quantity: 1,
-      type: PRODUCT
-    },
-    {
-      id: '10off',
-      productId: '10off',
-      code: '10off',
-      type: COUPON,
-      quantity: 1
-    }
-  ]
-  const context = {
-    log: {
-      error: () => {}
-    },
-    storage: {
-      user: {}
-    }
-  }
-
-  it('Should backup cart and remove original cart', async () => {
-    context.storage.user.get = async (key) => {
-      assert.equal(key, 'cart')
-      return cart
-    }
-    context.storage.user.set = async (key, val) => {
-      assert.equal(key, 'cart_bak')
-      assert.deepEqual(val, cart)
-    }
-    context.storage.user.del = async (key) => {
+  it('Should delete the cart', async () => {
+    const delHandler = async (key) => {
       assert.equal(key, 'cart')
     }
     try {
       // noinspection JSCheckFunctionSignatures
-      await executeStep(context, {cartStorageName})
+      await executeStep(createContext(cartStorageName, null, null, null, delHandler), createInput(cartStorageName))
     } catch (err) {
+      // not subject of the test, should therefore not fail
+      assert.ifError(err)
+    }
+  })
+
+  it('Should throw an InternalError on storage issues', async () => {
+    const delHandler = async (key) => {
+      throw new Error()
+    }
+    try {
+      // noinspection JSCheckFunctionSignatures
+      await executeStep(createContext(cartStorageName, null, null, null, delHandler), createInput(cartStorageName))
+      assert.fail()
+    } catch (err) {
+      // not subject of the test, should therefore not fail
+      assert.ok(err instanceof InternalError)
+    }
+  })
+
+  it('Should delete the correct storage key', async () => {
+    let deleteKey
+    const delHandler = async (key) => {
+      deleteKey = key
+    }
+    try {
+      // noinspection JSCheckFunctionSignatures
+      await executeStep(createContext(cartStorageName, null, null, null, delHandler), createInput(cartStorageName))
+      assert.equal(deleteKey, 'cart')
+    } catch (err) {
+      // not subject of the test, should therefore not fail
+      assert.ifError(err)
+    }
+  })
+
+  it('Should delete the cart from the correct storage', async () => {
+    const storageName = {
+      user: 'user',
+      device: 'device'
+    }
+
+    // use mocked storage "set" functions (one for each storage)
+    const context = {}
+    const storageDestination = {}
+    let currentStorage = storageName.user
+    context[currentStorage] = createContext(storageName[currentStorage], null, null, null, async () => {
+      storageDestination[currentStorage] = storageName[currentStorage]
+    })
+    currentStorage = storageName.device
+    context[currentStorage] = createContext(storageName[currentStorage], null, null, null, async () => {
+      storageDestination[currentStorage] = storageName[currentStorage]
+    })
+
+    // execute step twice (to test both storage destinations) to run the test
+    try {
+      currentStorage = storageName.user
+      await executeStep(context[currentStorage], createInput(storageName[currentStorage]))
+      currentStorage = storageName.device
+      await executeStep(context[currentStorage], createInput(storageName[currentStorage]))
+
+      // make sure the right destination storage were used
+      assert(storageDestination.user === storageName.user && storageDestination.device === storageName.device)
+    } catch (err) {
+      // not subject of the test, should therefore not fail
       assert.ifError(err)
     }
   })
